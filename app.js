2b89b2b33608e01966f25536fc0d90371f2f8093
Goos <goos.bekerom@gmail.com>
2018-01-27 01:44:24 +0100

    Created base server and mvc structure

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to Database
+mongoose.connect(config.database);
+// On Connected
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database);
+});
+// On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ' + err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+const port = 3000;
+
+app.use(cors()); // To allow requests from other domains
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+// routes
+app.use('/users', users);
+
+// Index route
+app.get('/', (req, res) => {
+    res.send('Invalid endpoint');
+});
+
+app.listen(port, () => {
+    console.log('Server started on port: ' + port);
+});
