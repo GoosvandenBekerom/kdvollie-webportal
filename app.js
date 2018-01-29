f14228f4af754ce52409cde2c6dc22f72a1ba4df
Goos <goos.bekerom@gmail.com>
2018-01-29 21:47:56 +0100

    Added request logging, database connection and registration endpoint

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const config = require('./config/database');
 
 // Connect to Database
@@ -26,6 +27,9 @@ const port = 3000;
 app.use(cors()); // To allow requests from other domains
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+
+// Log incomming requests
+app.use(morgan('tiny'));
 
 // routes
 app.use('/users', users);
