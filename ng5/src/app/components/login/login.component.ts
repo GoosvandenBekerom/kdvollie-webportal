076956b11773475fc262f3a7f401168d9263a9b9
Goos <goos.bekerom@gmail.com>
2018-03-09 17:26:05 +0100

    Added login and logout functionality to angular app

--- ng5/src/app/components/login/login.component.ts
+++ ng5/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service'
+import { FlashMessagesService } from 'angular2-flash-messages'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  email: string
+  password: string
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private flash: FlashMessagesService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  onLoginSubmit() {
+    const user = {
+      email: this.email,
+      password: this.password
+    }
+
+    if (this.email == undefined || this.password == undefined) {
+      this.flashDanger('Vul eerst uw emailadres en wachtwoord in')
+      return false
+    }
+
+    this.authService.authenticate(user).subscribe(data => {
+      console.log(data)
+      if (data.success) {
+        this.authService.storeUserData(data.token, data.user)
+        this.flashSuccess('U bent succesvol ingelogd')
+        this.router.navigate(['dashboard'])
+      } else {
+        this.flashDanger(data.msg)
+      }
+    });
   }
 
+  private flashDanger(message: string) {
+    this.flash.show(message, {cssClass: 'alert-danger', timeout: 5000})
+  }
+
+  private flashSuccess(message: string) {
+    this.flash.show(message, {cssClass: 'alert-success', timeout: 5000})
+  }
 }
--- ng5/src/app/services/auth.service.ts
+++ ng5/src/app/services/auth.service.ts
@@ -7,14 +7,36 @@ import { User } from '../models/User'
 @Injectable()
 export class AuthService {
   authToken: any
-  user: User
+  user: any
 
   constructor(private http: Http) { }
 
   registerUser(user: User) {
+    return this.http.post('http://localhost:3000/user/register', user, {headers: this.getHeaders()})
+      .map(res => res.json())
+  }
+
+  authenticate(credentials: any) {
+    return this.http.post('http://localhost:3000/user/authenticate', credentials, {headers: this.getHeaders()})
+      .map(res => res.json())
+  }
+
+  private getHeaders() {
     let headers = new Headers()
     headers.append('Content-Type', 'application/json')
-    return this.http.post('http://localhost:3000/user/register', user, {headers: headers})
-      .map(res => res.json())
+    return headers
+  }
+
+  storeUserData(token: any, user: any) {
+    localStorage.setItem('id_token', token)
+    localStorage.setItem('user', JSON.stringify(user))
+    this.authToken = token
+    this.user = user
+  }
+
+  logout() {
+    this.authToken = null
+    this.user = null
+    localStorage.clear()
   }
 }
