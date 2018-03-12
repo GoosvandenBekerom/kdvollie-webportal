a05aa67e7fbd5150437ba2357a2c52eb25a1132e
Goos <goos.bekerom@gmail.com>
2018-03-12 23:45:36 +0100

    Started templating homepage

--- ng5/src/app/components/home/home.component.ts
+++ ng5/src/app/components/home/home.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  page: Object
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.page = {
+      company: {
+        name: "Kinderdagverblijf Ollie & Co",
+        address: "Oude Oostrumseweg 26",
+        postal: "5802CC Venray",
+        phone: "0478-579475",
+        email: "kdvollie@gmail.com"
+      },
+      banner: "building.jpg",
+      quote: "Ondanks dat wij bij Ollie & Co nederlands spreken, vindt onze naam zijn oorsprong in het Venrayse dialect. “wej brenge um bej ollie” (we brengen hem naar jullie)",
+      card1: {
+        title: "Kaart 1",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.",
+        action: "#",
+        actionText: "Meer informatie"
+      },
+      card2: {
+        title: "Kaart 2",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.",
+        action: "#",
+        actionText: "Meer informatie"
+      },
+      card3: {
+        title: "Kaart 3",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.",
+        action: "#",
+        actionText: "Meer informatie"
+      }
+    }
   }
-
 }
